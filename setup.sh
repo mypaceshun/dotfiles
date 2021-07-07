829f98a148355cecaabc770e30ca55ceea3b4233
KAWAI Shun <shun@osstech.co.jp>
2021-07-07 12:59:46 +0900

    エラーでも止めない

--- setup.sh
+++ setup.sh
@@ -31,5 +31,5 @@ done
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -vs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" || :
 done
