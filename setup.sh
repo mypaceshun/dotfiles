50cba13dfa6bd4d8f3f1323ae7983ce7ab27c4f0
KAWAI Shun <shun@osstech.co.jp>
2022-01-25 17:37:24 +0900

    .zprezto との別れ

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASEDIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-DOT_FILES=(.bash* .vim* .zsh* .zpreztorc .gitconfig)
+DOT_FILES=(.bash* .vim* .zsh* .gitconfig)
 
 for file in ${DOT_FILES[@]}
 do
