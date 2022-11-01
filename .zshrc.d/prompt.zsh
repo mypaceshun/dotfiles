36676abc528b8f2ec08aff2aee06a335489acad2
KAWAI Shun <shun@osstech.co.jp>
2022-11-01 15:21:54 +0900

    remove precmd

--- .zshrc.d/prompt.zsh
+++ .zshrc.d/prompt.zsh
@@ -18,22 +18,3 @@ else
 [%{$fg[red]%}%n@%{$fg[green]%}%m%{$reset_color%}] $(__git_ps1 "(%s)")
 %{$fg[cyan]%}%~%{$reset_color%}$ '
 fi
-
-function precmd () {
-  FETCH_RUN=~/.fetch_run
-  DELAY_TIME=10
-  git rev-parse --git-dir >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    # git repositrory
-    _GIT_REMOTE=`git remote`
-    if [ "${_GIT_REMOTE}" != "" ]; then
-      NOW_TIME=`date +%s`
-      OLD_TIME=`cat ${FETCH_RUN} 2>/dev/null || echo ""`
-      NEXT_TIME=$(( ${OLD_TIME} + ${DELAY_TIME} ))
-      if [ ${NEXT_TIME} -lt ${NOW_TIME} ]; then
-	date +%s > ${FETCH_RUN}
-	git fetch >/dev/null 2>&1 &!
-      fi
-    fi
-  fi
-}
