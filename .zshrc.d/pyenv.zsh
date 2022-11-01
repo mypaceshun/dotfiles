0c1c3b5144f7432f80dc7a8c7ac1bba29ea383a5
KAWAI Shun <shun@osstech.co.jp>
2022-11-01 15:38:44 +0900

    Update pyenv.zsh

--- .zshrc.d/pyenv.zsh
+++ .zshrc.d/pyenv.zsh
@@ -3,4 +3,5 @@ if [ -e "$HOME/.pyenv" ]; then
     export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init --path)"
+    eval "$(pyenv virtualenv-init -)"
 fi
