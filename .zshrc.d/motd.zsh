b94e9d5c319794ee2ac300d106a1e0a46682d890
KAWAI Shun <shun@osstech.co.jp>
2022-11-02 11:29:07 +0900

    motd.zshは基本使わない

--- .zshrc.d/motd.zsh
+++ .zshrc.d/motd.zsh
@@ -1,6 +1,6 @@
-MOTD_DIR=/etc/update-motd.d
-
-if [ -d ${MOTD_DIR} ]; then
-  echo ""
-  run-parts ${MOTD_DIR}
-fi
+# MOTD_DIR=/etc/update-motd.d
+# 
+# if [ -d ${MOTD_DIR} ]; then
+#   echo ""
+#   run-parts ${MOTD_DIR}
+# fi
