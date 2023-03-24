f6ee1d6e5256c6ef6ca25dd2d9a1302152f040c7
KAWAI Shun <shun@osstech.co.jp>
2023-03-24 12:17:35 +0900

    Add nvm use node

--- .zshrc.d/nvm.zsh
+++ .zshrc.d/nvm.zsh
@@ -2,3 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+command -v nvm > /dev/null
+if [ $? -eq 0 ]; then
+  nvm use node > /dev/null
+fi
