b01973f52f8b8cbceb0845121a42ef95013d4a63
KAWAI Shun <shun@localhost.localdomain>
2025-01-20 16:13:34 +0900

    update zsh-completion

--- setup.zsh
+++ setup.zsh
@@ -2,7 +2,7 @@ BASEDIR=$(cd $(dirname $0); pwd)
 COMPDIR=~/.zsh/Completion
 
 # download zsh-completions
-ZSH_COMPLETIONS_TAG=0.33.0
+ZSH_COMPLETIONS_TAG=0.35.0
 if [ ! -e ${COMPDIR}/zsh-completions-${ZSH_COMPLETIONS_TAG} ]; then
   curl -L https://github.com/zsh-users/zsh-completions/archive/refs/tags/${ZSH_COMPLETIONS_TAG}.tar.gz -o ${BASEDIR}/zsh-completions.tar.gz
   tar xf ${BASEDIR}/zsh-completions.tar.gz -C ${COMPDIR}
