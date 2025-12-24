68c4d89d389e5049044dfd55bd51b61365416291
KAWAI Shun <mypaceshun@gmail.com>
2025-12-24 16:58:56 +0900

    PROMPT Add date and exec time

--- .zshrc.d/prompt.zsh
+++ .zshrc.d/prompt.zsh
@@ -9,12 +9,26 @@ if [ -f ${git_promptpath} ]; then
   export GIT_PS1_SHOWUPSTREAM=auto
 fi
 
+function preexec() {
+  CMD_START_TIME=$(date +%s.%N)
+}
+
+function precmd() {
+  if [[ -n ${CMD_START_TIME} ]]; then
+    local endtime=$(date +%s.%N)
+    CMD_ELAPSED_TIME=$(printf "%.2f" "$(( endtime - CMD_START_TIME ))")
+    unset CMD_START_TIME
+  fi
+}
+
 if [ ${UID} -eq 0 ]; then
   PROMPT='
 <%{$fg[red]%}%n@%{$fg[green]%}%m%{$reset_color%}> $(__git_ps1 "(%s)")
-%{$fg[cyan]%}%~%{$reset_color%}# '
+%{$fg[cyan]%}%1d%{$reset_color%}# '
 else
   PROMPT='
 [%{$fg[red]%}%n@%{$fg[green]%}%m%{$reset_color%}] $(__git_ps1 "(%s)")
-%{$fg[cyan]%}%~%{$reset_color%}$ '
+%{$fg[cyan]%}%1d%{$reset_color%}$ '
+
 fi
+RPROMPT='${CMD_ELAPSED_TIME}s %{$fg[yellow]%}%D{%Y-%m-%d %H:%M:%S}%{$reset_color%}'
